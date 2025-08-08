beb3790e919b7b7efbf1515f02ddef6cfe624872
IgnacioVeiga <ignacio.veiga29@gmail.com>
2025-08-07 21:04:26 -0300

    Formatear rutas en app.routes.ts para mejorar la legibilidad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,14 +8,44 @@ import { ClientsComponent } from './clients/clients.component';
 import { ClientFormComponent } from './clients/client-form/client-form.component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent, title: 'Panel principal' },
-    { path: 'tickets', component: TicketsComponent, title: 'Tickets' },
-    { path: 'tickets/nuevo', component: TicketFormComponent, title: 'Nuevo Ticket' },
-    { path: 'tickets/:id', component: TicketFormComponent, title: 'Detalle de Ticket' },
-    { path: 'inventario', component: InventoryComponent, title: 'Inventario' },
-    { path: 'inventario/nuevo', component: InventoryFormComponent, title: 'Alta de Componente' },
-    { path: 'inventario/:id', component: InventoryFormComponent, title: 'Detalle de Componente' },
-    { path: 'clientes', component: ClientsComponent, title: 'Clientes' },
-    { path: 'clientes/nuevo', component: ClientFormComponent, title: 'Alta de Cliente' },
-    { path: 'clientes/:id', component: ClientFormComponent, title: 'Detalle de Cliente' },
+    {
+        path: '',
+        component: DashboardComponent, title: 'Panel principal'
+    },
+    {
+        path: 'tickets',
+        component: TicketsComponent, title: 'Tickets'
+    },
+    {
+        path: 'tickets/nuevo',
+        component: TicketFormComponent, title: 'Nuevo Ticket'
+    },
+    {
+        path: 'tickets/:id',
+        component: TicketFormComponent, title: 'Detalle de Ticket'
+    },
+    {
+        path: 'inventario',
+        component: InventoryComponent, title: 'Inventario'
+    },
+    {
+        path: 'inventario/nuevo',
+        component: InventoryFormComponent, title: 'Alta de Componente'
+    },
+    {
+        path: 'inventario/:id',
+        component: InventoryFormComponent, title: 'Detalle de Componente'
+    },
+    {
+        path: 'clientes',
+        component: ClientsComponent, title: 'Clientes'
+    },
+    {
+        path: 'clientes/nuevo',
+        component: ClientFormComponent, title: 'Alta de Cliente'
+    },
+    {
+        path: 'clientes/:id',
+        component: ClientFormComponent, title: 'Detalle de Cliente'
+    }
 ];
